18b61fe792d8aa140785290c45b83db67a639037
toto4890 <simone.ozzello@gmail.com>
2015-10-20 22:32:27 +0000

    initial commit

--- scope.js
+++ scope.js
@@ -5,13 +5,14 @@ var a = 1, b = 2, c = 3;
 
     (function secondFunction(){
         var b = 8;
-
+        console.log("a: "+a+", b: "+b+", c: "+c);
         (function thirdFunction(){
             var a = 7, c = 9;
+            
 
             (function fourthFunction(){
                 var a = 1, c = 8;
-
+                
             })();
         })();
     })();
